0a362ad8e11bfec9aa88b7dc44d0c3b7d435e65b
12030819 Elliot <elliot.torrano@gmail.com>
2017-03-07 22:12:14 -0600

    Dialogo/frijol

--- practicas/practica 7/js/loteria.js
+++ practicas/practica 7/js/loteria.js
@@ -3,30 +3,127 @@ $(document).ready(
 	function(){
 		console.log("HOLIWIS DESDE JQUERY");
 		//alert("HOLIWIS DESDE JQUERY");
-		$("img").click(
+		/*$("img").click(
 			function(){
 				//$(this).width("150px");
 				//$(this).hide("slow");
-				$(this).attr('src','images/frijol.png');
+				$("#img1").show("fast");
 
 			}
-		);
+		);*/
+        var i=0;
+        
+        $("#i2").click(
+            function(){
+                i++;
+                $("#fr2").show("fast");
+                if(i==9){
+                    alert("GANASTE");
+                }
+            }
+        );
+        
+        $("#i3").click(
+            function(){
+                i++;
+                $("#fr3").show("fast");
+                if(i==9){
+                    alert("GANASTE");
+                }                
+            }
+        );
+        
+        $("#i4").click(
+            function(){
+                i++;
+                $("#fr4").show("fast");
+                if(i==9){
+                    alert("GANASTE");
+                }                
+            }
+        );
+        
+        $("#i5").click(
+            function(){
+                i++;
+                $("#fr5").show("fast");
+                if(i==9){
+                    alert("GANASTE");
+                }                
+            }
+        );
+        
+        $("#i6").click(
+            function(){
+                i++;
+                $("#fr6").show("fast");
+                if(i==9){
+                    alert("GANASTE");
+                }                
+            }
+        );
+        
+        $("#i7").click(
+            function(){
+                i++;
+                $("#fr7").show("fast");
+                if(i==9){
+                    alert("GANASTE");
+                }                
+            }
+        );
+        
+        $("#i8").click(
+            function(){
+                i++;
+                $("#fr8").show("fast");
+                if(i==9){
+                    alert("GANASTE");
+                }                
+            }
+        );
+        
+        $("#i9").click(
+            function(){
+                i++;
+                $("#fr9").show("fast");
+                if(i==9){
+                    alert("GANASTE");
+                }                
+            }
+        );
+        
+        $("#i0").click(
+            function(){
+                i++;
+                $("#fr0").show("fast");
+                if(i==9){
+                    alert("GANASTE");
+                }                
+            }
+        );
+        
 		$("#restart").click(
 			function(){
-				$("img").show("fast");
+				$("#fr2").hide("slow");
+                $("#fr3").hide("slow");
+                $("#fr4").hide("slow");
+                $("#fr5").hide("slow");
+                $("#fr6").hide("slow");
+                $("#fr7").hide("slow");
+                $("#fr8").hide("slow");
+                $("#fr9").hide("slow");
+                $("#fr0").hide("slow");
+                i=0;
 			}
-
 		);
 
-		var myChange=setInterval(change,1000);
-		
-		function change(){
-			console.log(parseInt(Math.random()*9+1))
-			$("#prin").attr('src','images/'+parseInt(Math.random()*9+1)+'.png');
-			
+		var myChange=setInterval(rand, 500), array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], i, leng;
+        
+        function rand() {
+            $("#prin").attr('src','images/'+parseInt(Math.random()*10)+'.png');		
 		}
-		
-
+    
 	}
 );
 
